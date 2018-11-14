b9f3a8f6c9053735a02f1097f7366b43ad09358f
Michał Jakubiec <michalpjakubiec@gmail.com>
2018-11-14 17:28:24 +0100

    Add project files.

---
+++ 20181114/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _20181114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = "input.txt";
+
+            IList<string> inputLines = ReadLinesToList(filePath);
+
+            var regexPatterns = new List<Regex>
+            {
+                new Regex (@"(^\+|^-|^\*|^\/)(.*)"),         // operator
+                new Regex (@"(^\(|^\))(.*)"),                // parentheses
+                new Regex (@"(^\d+.\d+)(.*)"),               // float
+                new Regex (@"(^\d+)(.*)"),                   // integer
+                new Regex (@"(^[A-Za-z][A-Za-z0-9]*)(.*)"),  // integer
+            };
+
+            foreach (var line in inputLines)
+            {
+                GetTokens(line, regexPatterns);
+            }
+        }
+
+        static void GetTokens(string line, IList<Regex> regexPatterns)
+        {
+            string remainingText = line;
+
+            for (int i = 0; i < regexPatterns.Count; i++)
+            {
+                regexPatterns[i].Match(remainingText);
+                regexPatterns[i].Split(remainingText).Select(x => x );
+            } 
+        }
+
+        private static IList<string> ReadLinesToList(string fileName)
+        {
+            IList<string> input = new List<string>();
+
+            if (File.Exists(fileName))
+            {
+                input = File.ReadLines(fileName).ToList();
+            }
+            else
+            {
+                Console.WriteLine("Input file not found. Quitting...");
+                Environment.Exit(2);
+            }
+
+            return input;
+        }
+    }
+}
---
+++ 20181114/readme.txt
@@ -0,0 +1,46 @@
+﻿Lekser
+Zmienić plik tekstowy z działaniami matematycznymi w strumień tokenów.
+3 metody do wyboru:
+- if'y
+- automat skończony
+- regex
+
+w pliku mogą być błędne dane, np.
+123.23.23
+123.a
+12x
+x12
+++
+( JJJJJJ )
+
+bez uwzględniania równań
+
+Rodzaje symboli leksykalnych (tokenów):
+operator (+ - * /)
+nawias ( ( ) )
+liczba całkowita
+liczba zmiennoprzecinkowa (z kropką)
+identyfikator (litera+cyfry lub litera) (identyfikator nie może zaczynać się od cyfr)
+
+np.
+x12+7.5
+identyfikator	x12
+operator plus	+
+liczba zmienno.	7.5
+
+123x+2.5
+3 7
+liczba całki	123
+
+x12+7.5
+123x+2.5
+3-7
+10 / 3
+x * y - z
+zmienna1 + zmienna2
+w1a*w2b
+10.3-alfa
+0.685739*5
+(x+y)*z
+(a * a) + b
+(0.1 - beta) + 8
